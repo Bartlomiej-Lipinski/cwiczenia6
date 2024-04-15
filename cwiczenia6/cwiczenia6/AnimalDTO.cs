d108235d862ac1dfa7d49e4b27f77027650254e1
rochmisz <109878606+rochmisz@users.noreply.github.com>
2024-04-16 01:12:52 +0200

    WIP-V2

--- cwiczenia6/cwiczenia6/AnimalDTO.cs
+++ cwiczenia6/cwiczenia6/AnimalDTO.cs
@@ -1,4 +1,7 @@
 ﻿namespace cwiczenia6;
 
-    public record GetAllAnimalsResponse(int ID, string Name, string Description, string Category, string Area);
-    public record CreateAnimalRequest(string Name, string Description, string Category, string Area);
+public record GetAllAnimalsResponse(int ID, string Name, string Description, string Category, string Area);
+
+public record CreateAnimalRequest(string Name, string Description, string Category, string Area);
+
+public record UpdateAnimalRequest(string Name, string Description, string Category, string Area);
--- cwiczenia6/cwiczenia6/Program.cs
+++ cwiczenia6/cwiczenia6/Program.cs
@@ -1,5 +1,6 @@
 using System.Data.SqlClient;
 using cwiczenia6;
+using Dapper;
 using FluentValidation;
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,12 +16,17 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapGet("/api/animals",()=>
+app.MapGet("/api/animals",(String? sortField)=>
 {
     var animals = new List<GetAllAnimalsResponse>();
     using (var sqlConnection = new SqlConnection("Server=localhost;Database=master;User Id=sa;Password=yourStrong(!)Password;"))
     {
-        var sqlCommand = new SqlCommand("SELECT * FROM Animal",sqlConnection );
+        String query = "SELECT * FROM Animal";
+        if (!string.IsNullOrEmpty(sortField))
+        {
+            query += $" ORDER BY {sortField}";
+        }
+        var sqlCommand = new SqlCommand(query,sqlConnection );
         sqlCommand.Connection.Open();
         var reader = sqlCommand.ExecuteReader();
         if (!reader.Read())
@@ -33,21 +39,6 @@ app.MapGet("/api/animals",()=>
         }
     }
     return Results.Ok(animals);
-});
-app.MapGet("/api/animals{id:int}",(int id)=>
-{
-    using (var sqlConnection = new SqlConnection("Server=localhost;Database=master;User Id=sa;Password=yourStrong(!)Password;"))
-    {
-        var sqlCommand = new SqlCommand("SELECT * FROM Animal where idAnimal=@id",sqlConnection );
-        sqlCommand.Parameters.AddWithValue("@id",id);
-        sqlCommand.Connection.Open();
-        var reader = sqlCommand.ExecuteReader();
-        if (!reader.Read())
-        {
-            return Results.NotFound();
-        }
-        return Results.Ok(new GetAllAnimalsResponse(reader.GetInt32(0),reader.GetString(1),reader.GetString(2),reader.GetString(3),reader.GetString(4)));
-    }
 });
 app.MapPost("/api/animals",(CreateAnimalRequest request,IValidator<CreateAnimalRequest> validator)=>
 {
@@ -68,7 +59,33 @@ app.MapPost("/api/animals",(CreateAnimalRequest request,IValidator<CreateAnimalR
     }
     return Results.Created("/api/animals",null);
 });
-
-
+app.MapPut("/api/animals{id:int}",(int id,UpdateAnimalRequest request,IValidator<UpdateAnimalRequest> validator)=>
+{
+    var validationResult = validator.Validate(request);
+    if (!validationResult.IsValid)
+    {
+        return Results.ValidationProblem(validationResult.ToDictionary());
+    }
+    using (var sqlConnection = new SqlConnection("Server=localhost;Database=master;User Id=sa;Password=yourStrong(!)Password;"))
+    {
+        var sqlCommand = new SqlCommand("UPDATE Animal SET Name=@Name,Description=@Description,Category=@Category,Area=@Area WHERE idAnimal=@id",sqlConnection );
+        sqlCommand.Parameters.AddWithValue("@id",id);
+        sqlCommand.Parameters.AddWithValue("@Name",request.Name);
+        sqlCommand.Parameters.AddWithValue("@Description",request.Description);
+        sqlCommand.Parameters.AddWithValue("@Category",request.Category);
+        sqlCommand.Parameters.AddWithValue("@Area",request.Area);
+        sqlCommand.Parameters.AddWithValue("@id",id);
+        sqlCommand.Connection.Open();
+        sqlCommand.ExecuteNonQuery();
+    }
+    return Results.Ok();
+});
+app.MapDelete("/api/animals{id:int}",(int id)=>
+{
+    using (var sqlConnection = new SqlConnection("Server=localhost;Database=master;User Id=sa;Password=yourStrong(!)Password;"))
+    {
+        sqlConnection.Execute("DELETE FROM ANIMAL WHERE idAnimal=@id",new { id });
+    }
+});
 app.Run();
 
